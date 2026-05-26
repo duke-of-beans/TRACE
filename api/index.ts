db6bbb427e1571ade67801eb95d4ec94ccaabe08
David Kirsch <dk.dkes@hotmail.com>
2026-05-26 00:26:55 -0700

    fix vercel api exports

--- api/index.ts
+++ api/index.ts
@@ -62,4 +62,9 @@ app.post("/feedback", async (c) => {
 app.use("/admin/*", operatorOnly);
 app.route("/admin", adminRouter);
 
-export default handle(app);
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
+export const OPTIONS = handle(app);
